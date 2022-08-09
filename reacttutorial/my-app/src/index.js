85fafd58f2172d79ec0ab07a8289bd99c5dfb859
vincent-cinardo <vcinardo@gmail.com>
2022-08-09 18:16:25 -0400

    Completed the tic tac toe game... Just need to add time travel to complete the tutorial.

--- reacttutorial/my-app/src/index.js
+++ reacttutorial/my-app/src/index.js
@@ -3,24 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 
-class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-
+/*class Square extends React.Component {
     render() {
       return (
         <button className="square" 
         onClick={() =>{
-          this.setState({value: 'X'});
+          this.props.onClick();
         }}>
-          {this.state.value}
+          {this.props.value}
         </button>
       );
     }
+  }*/
+
+  function Square(props) {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}  
+      </button>
+    );
   }
   
   class Board extends React.Component {
@@ -28,7 +29,20 @@ class Square extends React.Component {
       super(props);
       this.state = {
         squares: Array(9).fill(null),
+        xIsNext: true,
       };
+    }
+
+    handleClick(i) {
+      const squares = this.state.squares.slice();
+      if(calculateWinner(squares) || squares[i]) {
+        return;
+      }
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+      });
     }
 
     renderSquare(i) {
@@ -37,12 +51,21 @@ class Square extends React.Component {
           value={this.state.squares[i]}
           onClick={() => this.handleClick(i)}
         />
-      )
+      );
     }
   
     render() {
-      const status = 'Next player: X';
+
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      //const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
   
+      if(winner) {
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player' + (this.state.xIsNext ? 'X' : 'O');
+      }
+
       return (
         <div>
           <div className="status">{status}</div>
@@ -86,3 +109,25 @@ class Square extends React.Component {
   
   const root = ReactDOM.createRoot(document.getElementById("root"));
   root.render(<Game />);
+
+  //Helper function from 'taking turns'
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
